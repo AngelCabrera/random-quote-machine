6c5ae3cb8e4369b208d68616bb4991a0f8bcb482
Angel Cabrera <soyangelcabrera@gmail.com>
2019-02-13 12:57:04 -0400

    Updates

---
+++ precache-manifest.fc0e26c2c7f018d851c48004dde1eb30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a6e4adae3f8c7c8193b",
+    "url": "/random-quote-machine/static/js/runtime~main.1a6e4ada.js"
+  },
+  {
+    "revision": "9250084eb8c1d1231697",
+    "url": "/random-quote-machine/static/js/main.9250084e.chunk.js"
+  },
+  {
+    "revision": "7a12ac7fd3730a22d963",
+    "url": "/random-quote-machine/static/js/2.7a12ac7f.chunk.js"
+  },
+  {
+    "revision": "9250084eb8c1d1231697",
+    "url": "/random-quote-machine/static/css/main.89fd3d84.chunk.css"
+  },
+  {
+    "revision": "9170adcf9869827022dd0e3edebc7f16",
+    "url": "/random-quote-machine/index.html"
+  }
+];
